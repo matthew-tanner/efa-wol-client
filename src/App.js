b6bdaf100a8511d3336640b50bab96153b2a04f9
Matthew Tanner <115193+matthew-tanner@users.noreply.github.com>
2021-06-23 00:43:03 -0500

    sitebar core added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import chalk from "chalk";
+import { useState, useEffect } from 'react';
+import Sitebar from './Home/Sitebar';
+
+const log = (...args) => console.log(...args);
 
 function App() {
+  const [sessionToken, setSessionToken] = useState("");
+
+  useEffect(() => {
+    if(localStorage.getItem("token")){
+      setSessionToken(localStorage.getItem("token"))
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setSessionToken(newToken);
+    log("Info - new token", ":", updateToken.name, newToken );
+  }
+
   return (
     <div>
+      <Sitebar />
     </div>
   );
 }
